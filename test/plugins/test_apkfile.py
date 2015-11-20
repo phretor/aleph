702572841d106199236697d32e413c7c3fe58f76
Federico Maggi <federico.maggi@gmail.com>
2015-11-20 10:40:06 -0800

    added APK file plugin + test

---
+++ test/plugins/test_apkfile.py
@@ -0,0 +1,30 @@
+import unittest
+from mock import Mock
+from aleph.plugins.apkfile_plugin import _ as setup
+from aleph.plugins.apkfile_plugin import APKFilePlugin
+
+class PEInfoPluginTestCase(unittest.TestCase):
+
+    def test_it_is_initializable(self):
+        queue = Mock()
+        apk = APKFilePlugin(queue)
+        self.assertIsNotNone(apk)
+        self.assertIsInstance(apk, APKFilePlugin)
+
+    def test_it_get_apk_data(self):
+        queue = Mock()
+        sample = Mock()
+        apk = setup(queue)
+        sample.path = 'test/data/cld.navi.mainframe.apk'
+        apk.set_sample(sample)
+        data = apk.process()
+        import pprint; pprint.pprint(data)
+
+    def test_it_setup(self):
+        queue = Mock()
+        apk = setup(queue)
+        self.assertIsNotNone(apk)
+        self.assertIsInstance(apk, APKFilePlugin)
+
+if __name__ == '__main__':
+    unittest.main()
