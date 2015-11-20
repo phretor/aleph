c2dcef93deea81e1f67a296e875a89c63eb82afd
Federico Maggi <federico.maggi@gmail.com>
2015-11-20 15:45:02 -0800

    moved datastore under a module and added escaping submodule

--- aleph/datastore/elasticsearch_escape.py
+++ aleph/datastore/elasticsearch_escape.py
@@ -1,55 +1,56 @@
-import re
-
 """
 ElasticSearch >= 2.0.0 has declared some characters as illegal in field names. As such, we must escape field
 names before storing a document or before searching (trickier) and unescape them before sending the results
 back to the requesting method.
 
+We squash all the special characters to `_` (underscore).
+
 More details: https://github.com/elastic/elasticsearch/issues/6736
-
 """
-ILLEGAL_CHARS = ('\\', '/', '*', '?', '"', '<', '>', '|', ' ', '.')
 
-IC = re.compile(r'[%s]' % ''.join(ILLEGAL_CHARS))
-ICE = re.compile(r'[\\][%s]' % ''.join(ILLEGAL_CHARS))
+import re
+
+ILLEGAL_CHARS = (
+    chr(0x5c),  # \ backslash
+    chr(0x2f),  # / slash
+    chr(0x2a),  # * star
+    chr(0x3f),  # ? question mark
+    chr(0x22),  # " double quote
+    chr(0x3c),  # < less than
+    chr(0x3e),  # > greater than
+    chr(0x7c),  # | pipe
+    chr(0x20),  # space
+    chr(0x2e)   # . dot
+)
+IC = re.compile('[{}]'.format(''.join(ILLEGAL_CHARS)))
+SQUASH = '_'
+
 
 def escape(s):
-    """
-    :param s: string to escape
-    :return: string with `aleph.utils.elasticsearch.ILLEGAL_CHARS` backslash escaped (e.g., '.' -> '\.')
-
-    >>> escape('fi<>le"*name.|exe') == 'fi\<\>le\"\*name\.\|exe'
-    True
-    >>> escape('fi\<\>le\"\*name\.\|exe') == 'fi\<\>le\"\*name\.\|exe'
-    True
-    """
-    L = len(s)
-    escaped = ''
-    for i in xrange(L):
-        if IC.match(s[i]):
-            if i == 0 or s[i] != '\\':
-                escaped += '\\{}'.format(s[i])
+    res = ''
+    for c in s:
+        if IC.match(c):
+            res += SQUASH
         else:
-            escaped += s[i]
-    return escaped
+            res += c
+    return res
 
-def unescape(s):
-    """
-    :param s: string to escape
-    :return: string with escaped `aleph.utils.elasticsearch.ILLEGAL_CHARS` unescaped (e.g., '\.' -> '.')
-
-    >>> unescape('fi\<\>le\"\*name\.\|exe') == 'fi<>le"*name.|exe'
-    True
-    >>> unescape('fi<>le"*name.|exe') == 'fi<>le"*name.|exe'
-    True
-    """
-    return s.replace('\\', '')
 
 def escape_doc(doc):
     """
-    Recursively descent a dictionary an escape the keys if necessary.
+    Recursively descent a dictionary an escape the keys.
+
     :param doc: a dictionary
     :return: a dictionary with field names escaped as per `aleph.utils.elasticsearch.escape`
-    escape_doc({'doc': {'fi<>le"*name.|exe': ['fi<>le"*name.|exe'], ''}})
     """
-    return escaped_doc
+    ret = {}
+    for k, v in doc.iteritems():
+        if IC.match(k):
+            K = escape(k)
+        else:
+            K = k
+        if isinstance(v, dict):
+            v = escape_doc(v)
+
+        ret[K] = v
+    return ret
